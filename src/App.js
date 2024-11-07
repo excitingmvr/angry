6d0ca694b06a6e295ca24b16ea1370935289f147
excitingmvr <excitingmvr@gmail.com>
2024-11-07 13:52:27 +0900

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          123Learn Reactttt
         </a>
       </header>
     </div>
